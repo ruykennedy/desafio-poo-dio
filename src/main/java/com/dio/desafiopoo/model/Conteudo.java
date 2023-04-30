40eff43a8c5f49f4de59735afb586388af3f9f2a
ruykennedy <ruy-kennedy@hotmail.com>
2023-04-30 10:20:30 -0300

    deasafio poo

---
+++ src/main/java/com/dio/desafiopoo/model/Conteudo.java
@@ -0,0 +1,21 @@
+package com.dio.desafiopoo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Conteudo {
+
+    private String titulo;
+    private String descricao;
+    private int xp;
+
+    public Conteudo(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.xp = 0;
+    }
+
+    public abstract void calcularXp();
+}
